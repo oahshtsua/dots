cbe06865943302fab398377d28e2a71e27c2f6ec
Aashutosh A. <ashuotash@mailbox.org>
2024-06-13 08:17:33 +0545

    nvim: remove lsp keymaps now set by default

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -106,12 +106,9 @@ end
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 	callback = function(event)
-		-- Enable omnicomplete
-		vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 		-- Buffer local mappings.
 		-- See :help vim.lsp.* for documentation on any of the below functions
 		local opts = { buffer = event.buf }
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
